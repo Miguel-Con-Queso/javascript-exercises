9acc495e242ea79a62d0132255ab58b5d413d19a
Michael Pinkston <mkpinkston@my.milligan.edu>
2023-05-16 15:38:43 -0400

    completed questions 6 & 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fTemp) {
+  let tempC = (fTemp - 32) * (5 / 9);
+
+  let newCelTemp = Math.round(tempC * 10) / 10;
+  return newCelTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cTemp) {
+  let tempF = cTemp * (9 / 5) + 32;
+
+  let newFahrTemp = Math.round(tempF * 10) / 10;
+  return newFahrTemp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
